1785f4886f5903f4b5d2ca4d3845d0897b01b636
princejackchang <princejackchange@gmail.com>
2015-05-03 14:22:47 +0800

    * commit the answer to exercise 2.2

---
+++ chapter2/exercise-2-2.scm
@@ -0,0 +1,29 @@
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p) (car p))
+
+(define (y-point p) (cdr p))
+
+(define (make-segment pt1 pt2)
+  (cons pt1 pt2))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (midpoint-segment segment)
+  (make-point (/ (+ (x-point (start-segment segment))
+					(x-point (end-segment segment))) 2)
+			  (/ (+ (y-point (start-segment segment))
+					(y-point (end-segment segment))) 2)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
