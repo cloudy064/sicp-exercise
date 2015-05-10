f6aafdc675cf668ab4b995862f613bb62e782052
princejackchang <princejackchang@gmail.com>
2015-05-10 19:27:38 +0800

    * commit the answer to exercise 2.12

---
+++ chapter2/exercise-2-12.scm
@@ -0,0 +1,14 @@
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (make-interval (- c p) (+ c p)))
+
+(define (percent p)
+  (/ (- (upper-bound p) (lower-bound p)) 2))
