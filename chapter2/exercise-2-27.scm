f876a87ce4d2a769d38f42dbc2b0b43c5db8f4e6
princejackchang <princejackchang@gmail.com>
2015-05-11 13:58:54 +0800

    * add some comments to exercise-2-27.scm * provide a better solution to exercise 2.27

--- chapter2/exercise-2-27.scm
+++ chapter2/exercise-2-27.scm
@@ -14,3 +14,16 @@
 			  (deep-reverse-iter (cons (reverse-list element) result) other)
 			  (deep-reverse-iter (cons element result) other))))
 	(deep-reverse-iter () tree)))
+
+;; Also, you can write this procedure just like this:
+;; (define (deep-reverse tree)
+;;   (if (pair? tree)
+;;     (reverse (map deep-reverse tree))
+;;     tree))
+;; You can think of this problem as follow.
+;; For example, we have to reverse the tree
+;;          (list 1 2 (list 2 3 4) 3 4 (list 1 2))
+;; We can reverse every subtree first(if it is a leaf, the reverse of it is itself),
+;; namely apply deep-reverse to each element of tree. (map deep-reverse tree)
+;; And then, we reverse the result of these elements. (reverse (map deep-reverse tree))
+;; And now, we exactly get the result of (deep-reverse tree).
