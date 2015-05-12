8d74b52599ac14e21582902c919f189bb09ea5ea
princejackchang <princejackchang@gmail.com>
2015-05-12 20:44:31 +0800

    * add some comments to exercise 2.32

--- chapter2/exercise-2-32.scm
+++ chapter2/exercise-2-32.scm
@@ -1,3 +1,10 @@
+;; We can consider this problem as follow:
+;; We can divide set S into two parts,
+;; one part with (car s), the other without (car s).
+;; So the subsets of S is just the combination of
+;; subsets of the first part and the on of the second part.
+;; And then we can define this procedure like this.
+
 (define (subsets s)
   (if (null? s)
 	  (list s)
