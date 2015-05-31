c0d9729784caf79d8f3a31bd520756c38cac89c3
princejackchang <princejackchang@gmail.com>
2015-05-31 14:54:40 +0800

    * add prime? function the public.scm

--- chapter1/public.scm
+++ chapter1/public.scm
@@ -29,3 +29,23 @@
 
 (define (average-three a b c)
   (/ (+ a b c) 3))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
