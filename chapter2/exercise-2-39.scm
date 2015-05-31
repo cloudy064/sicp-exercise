f0116a64961b2b015d0270378adb16c75f359d20
princejackchang <princejackchang@gmail.com>
2015-05-31 13:11:22 +0800

    * commit the answer to exercise 2.39

---
+++ chapter2/exercise-2-39.scm
@@ -0,0 +1,13 @@
+(load "exercise-2-38.scm")
+
+(define (reverse-right sequence)
+  (fold-right
+   (lambda (x y) (append y (list x)))
+   ()
+   sequence))
+
+(define (reverse-left sequence)
+  (fold-left
+   (lambda (x y) (cons y x))
+   ()
+   sequence))
