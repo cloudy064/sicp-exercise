dfae0025b61823f7f71507be27a9c97075ec1869
princejackchang <princejackchang@gmail.com>
2015-06-01 21:02:28 +0800

    * commit the answer to exercise 2.46 and 2.47

---
+++ chapter2/exercise-2-46.scm
@@ -0,0 +1,29 @@
+(define (frame-coord-map frame)
+  (lambda (v)
+	(add-vect
+	 (origin-frame frame)
+	 (add-vect (scale-vect (xcor-vect v)
+						   (edge1-frame frame))
+			   (scale-vect (ycor-vect v)
+						   (edge2-frame frame))))))
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+			 (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+			 (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+			 (* s (ycor-vect v))))
