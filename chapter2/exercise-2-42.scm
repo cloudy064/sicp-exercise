cefa8bd2e1d87c4054fec89e6a6cfa49ecae20a7
princejackchang <princejackchang@gmail.com>
2015-06-01 13:59:10 +0800

    * commit the answer to exercise 2.42

---
+++ chapter2/exercise-2-42.scm
@@ -0,0 +1,44 @@
+(load "public.scm")
+(load "../chapter1/public.scm")
+
+(define (queens board-size)
+  (define (queen-cols k)
+	(if (= k 0)
+		(list empty-board)
+		(filter
+		 (lambda (positions) (safe? k positions))
+		 (flatmap
+		  (lambda (rest-of-queens)
+			(map (lambda (new-row)
+				   (adjoin-position new-row k rest-of-queens))
+				 (enumerate-interval 1 board-size)))
+		  (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+;; Just notice here that, the positions we generate are inverted.
+;; So, when we test the safety of positions, we just pick the first element of the list as the k-th column position.
+(define (adjoin-position row col rest-of-queens)
+  (cons (list col row) rest-of-queens))
+
+(define (safe? k positions)
+  (if (null? positions)
+	  #t
+	  (let ((check-col (car positions))
+			(rest-cols (cdr positions)))
+		(define (safe-iter rest)
+		  (if (null? rest)
+			  #t
+			  (and (/= (+ (car check-col) (cadr check-col))
+					   (+ (car (car rest)) (cadr (car rest))))
+				   (/= (- (car check-col) (cadr check-col))
+					   (- (car (car rest)) (cadr (car rest))))
+				   (/= (cadr check-col) (cadr (car rest)))
+				   (safe-iter (cdr rest)))))
+		(safe-iter rest-cols))))
+
+(define empty-board ())
+
+;; I don't know the unequal symbol.
+;; So, I just define that here.
+(define (/= first second)
+  (not (= first second)))
